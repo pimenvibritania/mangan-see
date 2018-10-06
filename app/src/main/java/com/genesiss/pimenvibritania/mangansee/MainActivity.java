27249fece555abc4eb56dd07c044afc6d015143a
pimenvibritania <blackbrit13@gmail.com>
2018-10-06 17:55:09 +0700

    initial 2

--- app/src/main/java/com/genesiss/pimenvibritania/mangansee/MainActivity.java
+++ app/src/main/java/com/genesiss/pimenvibritania/mangansee/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(m);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.example_menu,menu);
+        return true;
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.option1:
+                Intent m = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(m);
+                break;
+            case R.id.option2:
+                Toast.makeText(this, "CLICKED", Toast.LENGTH_SHORT);
+                break;
+        }
+
+        return  true;
+
     }
 }
